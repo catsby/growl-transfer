31239beef607ae0354015c8fae7b866ee4a4a61c
Clint Shryock <clint@ctshryock.com>
2011-02-14 08:33:48 -0600

    Add @remote setup/clear in before/after
    Stubbed out some other feature

--- features/step_definitions/gt_scp_steps.rb
+++ features/step_definitions/gt_scp_steps.rb
@@ -1,11 +1,13 @@
 Before do
   FileUtils.mkdir(TEST_DIR)
   Dir.chdir(TEST_DIR)
+  @remote = []
 end
 
 After do
   Dir.chdir(TEST_DIR)
   FileUtils.rm_rf(TEST_DIR)
+  @remote.clear
 end
 
 class Output
@@ -26,7 +28,6 @@ def output
 end
 
 Given /^I have ssh keyless auth setup on "([^"]*)"$/ do |remote_host|
-  @remote = []
   @remote << remote_host
 end
 
@@ -38,6 +39,14 @@ Given /^I specify "([^"]*)" as the username before the url$/ do |arg1|
   @remote[0] = 'clint' + '@' + @remote[0]
 end
 
+Given /^I have a valid password setup on "([^"]*)"$/ do |remote_host|
+  pending # need server I have password but not keyless auth on @remote << remote_host
+end
+
+Given /^I specify the "([^"]*)" option$/ do |remote_host|
+  @remote << remote_host
+end
+
 When /^I run "([^"]*)"$/ do |arg1|
   GrowlTransfer::DEFAULTS.merge!({'output'=>output})
   GrowlTransfer::Download(@remote.join(':'), TEST_DIR)
@@ -47,6 +56,6 @@ Then /^I should see "([^"]*)"$/ do |status|
   output.messages.should include(status)
 end
 
-Then /^TEST_DIR should contains "([^"]*)" file$/ do |arg1|
+Then /^TEST_DIR should contain "([^"]*)" file$/ do |arg1|
   File.file?([TEST_DIR, arg1].join('/'))
 end
